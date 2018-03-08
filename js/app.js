34a4fe31bfae290e8a20a0935de1f316d88f71e2
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2018-03-08 08:11:36 +0200

    Creating array of objects that represent symbols instead of array of positions

--- js/app.js
+++ js/app.js
@@ -7,27 +7,43 @@ const symbol_files = ["img/ic_account_balance_black_24px.svg",
     const show_img = function(td, id) {
         $(td).append(`<img src="${card_symbol[parseInt(id)]}">`);
     }
-    let card_symbol = [];
+    const symbols = [];
     let count_of_moves = 0;
-    let one_card_flipped = -1;
+    let previous_clicked_card_id = null;
+    const completed_cards = []
 
     const dist_symbols_on_cards = function () {
+        const temp = [];
         for ( let i = 0; i < symbol_files.length; i++ ) {
+            const symbol = {
+                cards: [],
+                symbol_file: symbol_files[i],
+                completed: false
+            };
             for (let k = 0; k < 2; k++) {
                 let rand_try;
                 do {
                     rand_try = Math.floor(Math.random() * 16);
-                } while ( card_symbol[rand_try] !== undefined );
-                card_symbol[rand_try] = symbol_files[i];
+                } while ( temp[rand_try] !== undefined );
+                symbol.cards[k] = rand_try;
+                temp[rand_try] = 1;
             }
+            symbols[i] = symbol;
         }
     }
 
     $("table").on("click", "td", function (event) {
         const id = $(this).attr("id");
-        if ( one_card_flipped !== id ) {
-            show_img(this, id);
-            one_card_flipped = id;
+        if ( id !== previous_clicked_card_id ) {
+            if ( previous_clicked_card_id === null ) {
+                //previous_clicked_card_id = id;    // To compare in the next click
+            } else {
+                count_of_moves++;
+                previous_clicked_card_id = null;
+                if ( /* is_the_same_symbol */ true ) {
+                }
+            }
+
         }
     });
     dist_symbols_on_cards();
